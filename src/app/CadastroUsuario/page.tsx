547fe7aec497727b860bf42cf1a9e0db18a245e8
lucassalmeida03 <lucaspiresalmeida121@gmail.com>
2026-05-03 15:06:41 -0300

    feature: configuração de integração

--- src/app/CadastroUsuario/page.tsx
+++ src/app/CadastroUsuario/page.tsx
@@ -39,7 +39,7 @@ const CadastroUser = () => {
 
     try {
   const res = await axios.post(
-    `${process.env.NEXT_PUBLIC_MICRO_URL}/api/users/register`,
+    `${process.env.NEXT_PUBLIC_API_URL}/api/users`,
     novoUser,
     {
       headers: {
